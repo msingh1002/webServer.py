81d8a1a5cf9c5558f506a6a48341f1b0117cb661
msingh1002 <ms15355@nyu.edu>
2024-10-05 22:27:14 -0400

    Add files via upload

--- webServer.py
+++ webServer.py
@@ -55,7 +55,8 @@ def webServer(port=13331):
       #Send everything as one send command, do not send one line/item at a time!
 
       # Fill in start
-      connectionSocket.sendall(header + outputdata)
+      combination = header = outputdata
+      connectionSocket.sendall(combination)
 
       # Fill in end
         
